d8248a22e3d15fdf5fd5436244714804d8df20b8
unknown <satishpanchal651@gmail.com>
2023-03-16 20:59:39 +0530

    Connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import products from "./data/products.js";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 // initializing express with variable app
 const app = express();
@@ -31,5 +35,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} 
+  mode on port ${PORT}`.yellow.bold
+  )
 );
