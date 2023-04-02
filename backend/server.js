0dee9d742662e65b564875420cc811bd0ce517c3
unknown <satishpanchal651@gmail.com>
2023-04-02 22:35:17 +0530

    Started with Redux

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from "colors";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -23,6 +24,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
